ff7c8357b8b4b3cd063191bb92ccb5e36f6c58b6
Matt Darby <matt.darby@rackspace.com>
2016-06-28 16:53:34 -0400

    More variables

--- lib/openstack-ruby-sdk/services/nova/flavor.rb
+++ lib/openstack-ruby-sdk/services/nova/flavor.rb
@@ -1,8 +1,12 @@
-class OpenStackRubySDK::Nova::Flavor  
+class OpenStackRubySDK::Nova::Flavor
   include Peace::Model
+
   attr_accessor :id, :name, :links, :ram, :vcpus, :swap, :rxtx_factor, :disk
+
   attr_with_alias :extra_specs, 'OS-FLV-WITH-EXT-SPECS:extra_specs'
   attr_with_alias :ephemeral, 'OS-FLV-EXT-DATA:ephemeral'
+  attr_with_alias :disabled, 'OS-FLV-DISABLED:disabled'
+  attr_with_alias :is_public, 'os-flavor-access:is_public'
 
   validates :name, :links, presence: true
 
--- lib/openstack-ruby-sdk/services/nova/image.rb
+++ lib/openstack-ruby-sdk/services/nova/image.rb
@@ -1,8 +1,11 @@
-class OpenStackRubySDK::Nova::Image  
+class OpenStackRubySDK::Nova::Image
   include Peace::Model
-  attr_accessor :id, :name, :created, :updated, :state, :user_id, :tenant_id, :progress, :minDisk, :minRam, :disk_config, :links
+  
+  attr_accessor :id, :name, :created, :updated, :user_id, :tenant_id, :progress, :minDisk, :minRam, :disk_config, :links, :server, :metadata
 
   attr_with_alias :disk_config, 'OS-DCF:diskConfig'
+  attr_with_alias :size, 'OS-EXT-IMG-SIZE:size'
+  attr_with_alias :state, 'status'
 
   belongs_to :user
   belongs_to :tenant
