7b15e7490fc4ef24f16f94d52312e0483ebc817e
Matt Darby <matt.darby@rackspace.com>
2016-06-28 16:47:03 -0400

    Added more attribute aliases

--- lib/openstack-ruby-sdk/services/nova/server.rb
+++ lib/openstack-ruby-sdk/services/nova/server.rb
@@ -3,20 +3,27 @@ class OpenStackRubySDK::Nova::Server
 
   REBOOT_TYPES = ['SOFT', 'HARD']
 
-  attr_accessor :id, :name, :created, :updated, :progress, :user_id, :tenant_id, :links, :personality, :config_drive, :user_data, :addresses, :boot_volume_id, :boot_volume_size, :boot_image_id, :key_name, :metadata
+  attr_accessor :id, :name, :created, :updated, :progress, :user_id, :tenant_id, :links, :personality, :config_drive, :user_data, :addresses, :boot_volume_id, :boot_volume_size, :boot_image_id, :key_name, :metadata, :image, :flavor, :security_groups
 
+  attr_with_alias :availability_zone, 'OS-EXT-AZ:availability_zone'
   attr_with_alias :bandwidth, 'rax-bandwidth:bandwidth'
   attr_with_alias :disk_config, 'OS-DCF:diskConfig'
   attr_with_alias :flavor_id, :flavorRef
   attr_with_alias :host_id, :hostId
+  attr_with_alias :host, 'OS-EXT-SRV-ATTR:host'
+  attr_with_alias :hypervisor_hostname, 'OS-EXT-SRV-ATTR:hypervisor_hostname'
   attr_with_alias :image_id, :imageRef
+  attr_with_alias :instance_name, 'OS-EXT-SRV-ATTR:instance_name'
   attr_with_alias :ipv4_address, 'accessIPv4'
   attr_with_alias :ipv6_address, 'accessIPv6'
+  attr_with_alias :launched_at, 'OS-SRV-USG:launched_at'
   attr_with_alias :password, :adminPass
   attr_with_alias :power_state, 'OS-EXT-STS:power_state'
   attr_with_alias :state_ext, 'OS-EXT-STS:task_state'
   attr_with_alias :state, :status
+  attr_with_alias :terminated_at, 'OS-SRV-USG:terminated_at'
   attr_with_alias :vm_state, 'OS-EXT-STS:vm_state'
+  attr_with_alias :volumes_attached, 'os-extended-volumes:volumes_attached'
   attr_with_alias :zone_id, 'RAX-PUBLIC-IP-ZONE-ID:publicIPZoneId'
 
   belongs_to :user
