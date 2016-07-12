53c9ccfc54f54a48a77b4eb14e0b0bce6cd95e1d
Matt Darby <matt.darby@rackspace.com>
2016-07-12 16:01:52 -0400

    Nova::Server specs

--- spec/lib/openstack-ruby-sdk/services/nova/server_spec.rb
+++ spec/lib/openstack-ruby-sdk/services/nova/server_spec.rb
@@ -1,19 +1,26 @@
 require 'spec_helper'
 
 describe OpenStackRubySDK::Nova::Server, :vcr do
-  let(:server){ OpenStackRubySDK::Nova::Server.new }
+  let(:flavor_id) { '1' }
+  let(:image_id) { '9060f38f-bb54-4806-a2b3-24a2173af252' }
+  let(:server) do
+    OpenStackRubySDK::Nova::Server.create({
+      flavorRef: flavor_id,
+      imageRef: image_id,
+      name: 'Testing'
+    })
+  end
 
   it 'gets an index' do
     expect(OpenStackRubySDK::Nova::Server.all).to eq([])
   end
 
-  it 'gets its self' do
-    expect(OpenStackRubySDK::Nova::Server.find(server.id)).to eq(server)
+  it 'creates its self' do
+    expect(server.class).to eq(OpenStackRubySDK::Nova::Server)
   end
 
-  it 'creates its self' do
-    server.name = Time.now.usec.to_s
-    expect(server.save).to eq(true)
+  it 'gets its self' do
+    expect(OpenStackRubySDK::Nova::Server.find(server.id).id).to eq(server.id)
   end
 
   it 'updates its self' do
