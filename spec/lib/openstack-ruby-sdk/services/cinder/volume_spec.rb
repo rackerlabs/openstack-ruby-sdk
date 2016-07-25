f64dd37a119bcd30d155c3db0c368cca14d34729
Matt Darby <matt.darby@rackspace.com>
2016-07-25 11:55:16 -0400

    Cinder::Volume

--- spec/lib/openstack-ruby-sdk/services/cinder/volume_spec.rb
+++ spec/lib/openstack-ruby-sdk/services/cinder/volume_spec.rb
@@ -1,26 +1,32 @@
 require 'spec_helper'
 
 describe OpenStackRubySDK::Cinder::Volume, :vcr do
-  let(:volume){ OpenStackRubySDK::Cinder::Volume.new }
+  let(:volume) do
+    OpenStackRubySDK::Cinder::Volume.all.first
+  end
 
   it 'gets an index' do
-    expect(OpenStackRubySDK::Cinder::Volume.all).to eq([])
+    expect(OpenStackRubySDK::Cinder::Volume.all.count).to be >= 0
   end
 
   it 'gets its self' do
-    expect(OpenStackRubySDK::Cinder::Volume.find(volume.id)).to eq(volume)
+    expect(OpenStackRubySDK::Cinder::Volume.find(volume.id).id).to eq(volume.id)
   end
 
   it 'creates its self' do
-    volume.name = Time.now.usec.to_s
-    expect(volume.save).to eq(true)
+    v = OpenStackRubySDK::Cinder::Volume.new
+    v.name = Time.now.usec.to_s
+    v.size = '100'
+    v.save
+
+    expect(v.status).to eq('creating')
   end
 
   it 'updates its self' do
-    volume.name = Time.now.usec.to_s
-    expect(volume.save).to eq(true)
-    volume.name = Time.now.usec.to_s
-    expect(volume.save).to eq(true)
+    expect do
+      volume.name = Time.now.usec.to_s
+      volume.save
+    end.to change{ volume.name }
   end
 
   it 'deletes its self' do
