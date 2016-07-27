0c87af45c10340857439649d2c6bd03505096d1a
Matt Darby <matt.darby@rackspace.com>
2016-07-27 10:58:50 -0400

    Cinder::Volume

--- lib/openstack-ruby-sdk/services/cinder/volume.rb
+++ lib/openstack-ruby-sdk/services/cinder/volume.rb
@@ -2,9 +2,58 @@ class OpenStackRubySDK::Cinder::Volume
   include Peace::Model
   include Peace::Metadata
 
-  attr_accessor :attachments, :availability_zone, :bootable, :consistencygroup_id, :created_at, :description, :encrypted, :id, :links, :metadata, :migration_status, :multiattach, :name, :os_vol_host_attr, :os_vol_mig_status_attr, :os_vol_mig_status_attr, :os_vol_tenant_attr, :os_volume_replication, :os_volume_replication, :replication_status, :size, :snapshot_id, :source_volid, :status, :user_id, :volume_type, :display_name, :display_description
+  attr_accessor :attachments, :availability_zone, :bootable, :consistencygroup_id, :created_at, :description, :encrypted, :id, :links, :metadata, :migration_status, :multiattach, :name, :replication_status, :size, :snapshot_id, :source_volid, :status, :user_id, :volume_type, :display_name, :display_description
+
+  attr_with_alias :state, :status
+  attr_with_alias :tenant_id, "os-vol-tenant-attr:tenant_id"
+  attr_with_alias :tenant_host, "os-vol-tenant-attr:host"
+  attr_with_alias :tenant_name_id, "os-vol-tenant-attr:name_id"
+  attr_with_alias :tenant_migstat, "os-vol-tenant-attr:migstat"
+  attr_with_alias :host, "os-vol-host-attr:host"
+  attr_with_alias :migration_name_id, "os-vol-mig-status-attr:name_id"
+  attr_with_alias :migration_status, "os-vol-mig-status-attr:migstat"
 
   def detail
     self.reload
+  end
+
+  def extend_size(new_size)
+    perform_action!("os-extend": { new_size: new_size })
+  end
+
+  def reset_status(options={})
+    status  = options[:status] || "available"
+    attach  = options[:attach] || "detached"
+    migrate = options[:migrate] || "migrating"
+
+    data = Peace::Helpers.payload_builder("os-reset_status", {
+      status: status,
+      attach_status: attach,
+      migration_status: migrate
+    })
+
+    perform_action!(data)
+  end
+
+  def unmanage
+  end
+
+  def attach_to(server)
+    server.attach_volume(self.id)
+  end
+
+  def force_detach
+  end
+
+  def promote
+  end
+
+  def reenable_replication
+  end
+
+  private
+
+  def perform_action!(data)
+    Peace::Request.post("#{url}/action", data)
   end
 end
--- spec/lib/openstack-ruby-sdk/services/cinder/volume_spec.rb
+++ spec/lib/openstack-ruby-sdk/services/cinder/volume_spec.rb
@@ -1,9 +1,29 @@
 require 'spec_helper'
 
 describe OpenStackRubySDK::Cinder::Volume, :vcr do
-  let(:volume) do
-    OpenStackRubySDK::Cinder::Volume.all.first
+  let(:flavor_id) { '1' }
+  let(:image_id) { '7c5d126c-c468-4436-bfca-a001d9ea6fea' }
+  let(:server) do
+    s = OpenStackRubySDK::Nova::Server.create({
+      flavorRef: flavor_id,
+      imageRef: image_id,
+      name: Time.now.usec.to_s
+    })
+
+    Peace::Helpers.wait_for(s)
   end
+  let(:volume) do
+    v = OpenStackRubySDK::Cinder::Volume.create({
+      name: Time.now.usec.to_s,
+      size: '10'
+    })
+    Peace::Helpers.wait_for(v, 'available')
+  end
+
+  # after(:all) do
+  #   OpenStackRubySDK::Nova::Server.all.each{ |s| s.destroy }
+  #   OpenStackRubySDK::Cinder::Volume.all.each{ |v| v.destroy }
+  # end
 
   it 'gets an index' do
     expect(OpenStackRubySDK::Cinder::Volume.all.count).to be >= 0
@@ -16,7 +36,7 @@ describe OpenStackRubySDK::Cinder::Volume, :vcr do
   it 'creates its self' do
     v = OpenStackRubySDK::Cinder::Volume.new
     v.name = Time.now.usec.to_s
-    v.size = '100'
+    v.size = '10'
     v.save
 
     expect(v.status).to eq('creating')
@@ -29,7 +49,26 @@ describe OpenStackRubySDK::Cinder::Volume, :vcr do
     end.to change{ volume.name }
   end
 
+  it 'can be resized' do
+    expect do
+      volume.extend_size(11)
+      volume.reload
+    end.to change(volume, :size).to(11)
+  end
+
+  it 'can reset it statuses' do
+    expect do
+      volume.reset_status({status: 'creating'})
+      volume.reload
+    end.to change(volume, :status).to('creating')
+  end
+
+  it 'can attach its self to a server' do
+    volume.attach_to(server)
+  end
+
   it 'deletes its self' do
     expect(volume.destroy).to eq(true)
   end
+
 end
