1e74d594dfc02366aab48fac476975433280e289
Matt Darby <matt.darby@rackspace.com>
2016-07-28 15:02:23 -0400

    Cinder::VolumeSnapshot#update

--- spec/lib/openstack-ruby-sdk/services/cinder/volume_snapshot_spec.rb
+++ spec/lib/openstack-ruby-sdk/services/cinder/volume_snapshot_spec.rb
@@ -12,27 +12,19 @@ describe OpenStackRubySDK::Cinder::VolumeSnapshot, :vcr do
     expect(snapshot.destroy).to eq(true)
   end
 
-  # it 'gets an index' do
-  #   expect(OpenStackRubySDK::Cinder::VolumeSnapshot.all).to eq([])
-  # end
-  #
+  it 'gets an index' do
+    expect(OpenStackRubySDK::Cinder::VolumeSnapshot.all.count).to be >= 0
+  end
+
+  it 'updates its self' do
+    title = "Something different"
+    snapshot.name = title
+    snapshot.save
+    expect(snapshot.name).to eq(title)
+  end
+
   # it 'gets its self' do
   #   expect(OpenStackRubySDK::Cinder::VolumeSnapshot.find(volume_snapshot.id)).to eq(volume_snapshot)
   # end
   #
-  # it 'creates its self' do
-  #   volume_snapshot.name = Time.now.usec.to_s
-  #   expect(volume_snapshot.save).to eq(true)
-  # end
-  #
-  # it 'updates its self' do
-  #   volume_snapshot.name = Time.now.usec.to_s
-  #   expect(volume_snapshot.save).to eq(true)
-  #   volume_snapshot.name = Time.now.usec.to_s
-  #   expect(volume_snapshot.save).to eq(true)
-  # end
-  #
-  # it 'deletes its self' do
-  #   expect(volume_snapshot.destroy).to eq(true)
-  # end
 end
